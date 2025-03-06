f351fcaf5dcda120a60872533942fa4abc1a2e8e
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-03-06 22:19:45 +0100

    test(ui): update main_test to verify overview screen presence on app start

--- flutter_app/test/main_test.dart
+++ flutter_app/test/main_test.dart
@@ -1,11 +1,13 @@
 import 'package:app/main.dart';
-import 'package:flutter/material.dart';
+import 'package:app/presentation/pages/overview_screen.dart';
+import 'package:app/router/app_router.dart';
+import 'package:app/router/initialization_guard.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Flutter logo is present', (tester) async {
-    await tester.pumpWidget(const Main());
-
-    expect(find.byType(FlutterLogo), findsOneWidget);
+  testWidgets('$OverviewScreen is present on app start', (tester) async {
+    await tester.pumpWidget(Main(AppRouter(const InitializationGuard())));
+    await tester.pumpAndSettle();
+    expect(find.byType(OverviewScreen), findsOneWidget);
   });
 }
