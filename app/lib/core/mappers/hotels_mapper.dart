e2327adf8b0f54a869e15889236ebbc6813eb29c
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-03-07 20:38:04 +0100

    refactor: remove hotels mapper and simplify overview screen widget

--- app/lib/core/mappers/hotels_mapper.dart
+++
@@ -1,8 +0,0 @@
-import 'package:rest_api/hotels_api.dart' show Hotel, ModelMapper;
-
-class HotelsMapper extends ModelMapper<String, Hotel> {
-  const HotelsMapper();
-
-  @override
-  String mapItem(Hotel item) => item.images?.firstOrNull?.small ?? '';
-}
