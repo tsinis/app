b23b779a278785f8a05c166a0d4f72728bec9b7d
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-03-08 08:16:10 +0100

    refactor(logger): update logging calls to use new log interface

--- packages/logger/lib/src/logger_mixin.dart
+++ packages/logger/lib/src/logger_mixin.dart
@@ -2,19 +2,8 @@ import 'package:flutter/foundation.dart' show AsyncValueGetter, protected;
 import 'package:logging/logging.dart';
 
 mixin LoggerMixin {
-  Logger get _logger => Logger(super.runtimeType.toString());
-
   @protected
-  void info(String message) => log(Level.INFO, message);
-
-  @protected
-  // ignore: prefer-named-parameters, just a helper mixin.
-  void log(Level level, String message, [Object? error, StackTrace? stack]) =>
-      _logger.log(level, message, error, stack);
-
-  @protected
-  void severe(String message, [Object? error, StackTrace? stack]) =>
-      _logger.log(Level.SEVERE, message, error, stack ?? StackTrace.current);
+  Logger get log => Logger(super.runtimeType.toString());
 
   @protected
   Future<T?> tryOrLog<T extends Object>(
@@ -22,15 +11,15 @@ mixin LoggerMixin {
     String message, {
     T? orElse,
   }) async {
-    info('Ready to $message...');
+    log.info('Ready to $message...');
 
     try {
       final result = await future();
-      info('Attempt to $message was successful!');
+      log.info('Attempt to $message was successful!');
 
       return result;
     } catch (error, stackTrace) {
-      severe('Failed to $message!', error, stackTrace);
+      log.severe('Failed to $message!', error, stackTrace);
 
       return orElse;
     }
